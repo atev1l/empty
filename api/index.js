b40528930d97aafbf1aad12ec5f62e58d48bd2a1
atev1l <killfreedom@yandex.ru>
2024-04-21 12:38:17 +0300

    f7

--- api/index.js
+++ api/index.js
@@ -1,46 +1,12 @@
-const express = require('express');
-const Dropbox = require('dropbox').Dropbox;
-const dotenv = require('dotenv');
-const cors = require('cors');
-const helmet = require('helmet')
-const bodyParser = require('body-parser');
-
-dotenv.config();
+const express = require("express");
 
 const app = express();
 
-const dbx = new Dropbox({ accessToken: process.env.TOKEN });
-
-app.use(bodyParser.json());
-
-app.use(helmet())
-app.use(cors())
-app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', "script-src 'self' https://for-fun-gules.vercel.app");
-	res.setHeader('Access-Control-Allow-Origin', "*");
-  next();
+app.get("/a", (req, res) => {
+    res.send("Server example");
 });
-// app.post('/upload', (req, res) => {
 
-
-// 	const dataToSend = req.body.data;
-// 	const finger = req.body.finger
-
-//   dbx.filesUpload({ path: `/${finger}.txt`, contents: dataToSend })
-//     .then(response => {
-//       console.log('Файл успешно загружен в Dropbox:', response);
-//       res.send('Файл успешно загружен в Dropbox');
-//     })
-//     .catch(error => {
-//       console.error('Ошибка при загрузке файла в Dropbox:', error);
-//       res.status(500).send('Произошла ошибка при загрузке файла в Dropbox', error);
-//     });
-// });
-
-app.get('/a', (req, res) => {
-	res.json({message: "Hello from Express JS"})
-})
-
-app.listen(3001, () => console.log("Server ready on port 3001."));
-
-module.exports = app;
+// Initialize server
+app.listen(8080, () => {
+    console.log("Running on port 5000.");
+});
